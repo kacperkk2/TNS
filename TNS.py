1e137eae1e6c1e939acc622f3a374af06dde36ac
piter324 <muzyczukp@me.com>
2021-05-04 00:43:23 +0200

    Added experiments, their datasets and outputs; Added try/catch to expandL and expandR

--- TNS.py
+++ TNS.py
@@ -216,7 +216,12 @@ class TNS:
                         if left < self.dynamic_min_support:
                             break
                     elif len(tids_item_c) + left < self.dynamic_min_support:
-                        tids_item_c.remove(itemC)
+                        # print(itemC)
+                        try:
+                            tids_item_c.remove(itemC)
+                        except KeyError:
+                            pass
+                            # print(itemC, "not in set - exp L")
                         break
                     
                     if tids_item_c is None:
@@ -265,7 +270,12 @@ class TNS:
                         if left < self.dynamic_min_support:
                             continue
                     elif len(tids_item_c) + left < self.dynamic_min_support:
-                        tids_item_c.remove(itemC)
+                        # print(itemC)
+                        try:
+                            tids_item_c.remove(itemC)
+                        except KeyError:
+                            pass
+                            # print(itemC, "not in set - exp R")
                         continue
                     
                     if tids_item_c is None:
